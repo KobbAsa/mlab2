1cc681c04b2e14b20191d124c27b48636de9f194
Antonio <antony.dovzhenko@gmail.com>
2024-03-13 19:20:36 +0200

    Added format options

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 const fs = require('fs');
-const { convertMarkdownToHtml } = require('./converter');
+const { convertMarkdownToHtml, convertMarkdownToAnsi } = require('./converter');
 
 const inputFile = process.argv[2];
-const outputFile = process.argv[4];
+let outputFile;
+let outputFormat;
+
+process.argv.forEach((arg, index) => {
+    if (arg === '--out') {
+        outputFile = process.argv[index + 1];
+        outputFormat = 'html';
+    } else if (arg.startsWith('--format=')) {
+        outputFormat = arg.split('=')[1];
+    }
+});
 
 if (!inputFile) {
     console.error('Error: No input file specified');
@@ -16,17 +26,23 @@ fs.readFile(inputFile, 'utf8', (err, data) => {
     }
 
     try {
-        const html = convertMarkdownToHtml(data);
+        let output;
+        if (!outputFormat || outputFormat === 'ansi') {
+            output = convertMarkdownToAnsi(data);
+        } else if (outputFormat === 'html') {
+            output = convertMarkdownToHtml(data);
+        }
+
         if (outputFile) {
-            fs.writeFile(outputFile, html, (writeErr) => {
+            fs.writeFile(outputFile, output, (writeErr) => {
                 if (writeErr) {
                     console.error(`Error writing to file: ${outputFile}`, writeErr);
                     process.exit(1);
                 }
-                console.log(`HTML written to ${outputFile}`);
+                console.log(`Output written to ${outputFile}`);
             });
         } else {
-            console.log(html);
+            console.log(output);
         }
     } catch (conversionErr) {
         console.error(conversionErr.message);
