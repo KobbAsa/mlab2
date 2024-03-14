ae70b36939a9b292cfb87e66881d7b816b02b2f1
Antonio <antony.dovzhenko@gmail.com>
2024-03-14 17:15:01 +0200

    Added md to ansi converter

--- src/converter.js
+++ src/converter.js
@@ -41,6 +41,43 @@ function convertMarkdownToHtml(markdown) {
     return html;
 }
 
+const ANSI_BOLD = "\x1b[1m";
+const ANSI_ITALIC = "\x1b[3m";
+const ANSI_REVERSE = "\x1b[7m";
+const ANSI_RESET = "\x1b[0m";
+
+function convertMarkdownToAnsi(markdown) {
+    if (!checkForUnmatchedMarkers(markdown)) {
+        throw new Error('Error: Unmatched markdown markers found');
+    }
+
+    if (!checkForNesting(markdown)) {
+        throw new Error('Error: Nesting detected');
+    }
+
+    let ansiText = '';
+    const lines = markdown.split('\n');
+    let inPreBlock = false;
+
+    lines.forEach((line) => {
+        if (line.startsWith('```')) {
+            inPreBlock = !inPreBlock;
+            ansiText += inPreBlock ? ANSI_REVERSE : ANSI_RESET + '\n';
+        } else if (inPreBlock) {
+            ansiText += line + '\n';
+        } else {
+            line = line.replace(/\*\*(.*?)\*\*/g, ANSI_BOLD + '$1' + ANSI_RESET)
+                .replace(/_(.*?)_/g, ANSI_ITALIC + '$1' + ANSI_RESET)
+                .replace(/`(.*?)`/g, ANSI_REVERSE + '$1' + ANSI_RESET);
+
+            ansiText += line + '\n';
+        }
+    });
+
+    return ansiText + ANSI_RESET;
+}
+
+
 function checkForUnmatchedMarkers(markdown) {
     let isBoldOpen = false;
     let isMonospaceOpen = false;
@@ -118,4 +155,4 @@ function checkForNesting(markdown) {
     return true;
 }
 
-module.exports = { convertMarkdownToHtml };
+module.exports = { convertMarkdownToHtml, convertMarkdownToAnsi };
